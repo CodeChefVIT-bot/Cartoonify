3ec61c4c22d66808974da1488a9025bdeac7a212
deeppss <77568465+deeppss@users.noreply.github.com>
2021-08-04 13:20:41 +0530

    Add files via upload

---
+++ Source Code/functions/brightness.py
@@ -0,0 +1,5 @@
+def adjust_brightness(image, brightness):
+	invGamma = 1.0 / brightness
+	table = np.array([((i / 255.0) ** invGamma) * 255
+		for i in np.arange(0, 256)]).astype("uint8")
+	return cv2.LUT(image, table)
